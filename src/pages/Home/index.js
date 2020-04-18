b99710cc94c858a37fa134de49a2c6c88696cfe6
Gabriel "Dev HijaZi" F <46979459+devhijazi@users.noreply.github.com>
2020-04-18 20:18:55 -0300

    package fix

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 
 import { PieChart } from "react-native-svg-charts";
 
-import { ScrollView, Image, TouchableOpacity, Text } from "react-native";
+import { ScrollView, Image, TouchableOpacity, Text, View } from "react-native";
 import { Feather } from "@expo/vector-icons";
 
 import colors from "../../utils/Colors";
@@ -31,126 +31,54 @@ import {
 } from "./styles";
 
 export default function Home() {
-  const [status, setStatus] = useState({});
+  const [status, setStatus] = useState([]);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     async function loadBrazil() {
       const { data } = await api.get("brazil").then((r) => r.data);
 
       setStatus(data);
+      setData([
+        {
+          key: 1,
+          amount: data.confirmed,
+          svg: { fill: "#000" },
+        },
+        {
+          key: 2,
+          amount: data.cases,
+          svg: { fill: "#000" },
+        },
+        {
+          key: 3,
+          amount: data.deaths,
+          svg: { fill: "#000" },
+        },
+        {
+          key: 4,
+          amount: data.recovered,
+          svg: { fill: "#000" },
+        },
+      ]);
     }
     loadBrazil();
-  });
+  }, []);
+
+  if (!data.length) return <></>;
+  console.log(data);
 
   return (
     <Container>
-      <ScrollView showsVerticalScrollIndicator={false}>
-        <Header>
-          <HeaderText>COVID AGORA</HeaderText>
-          <DateText>{moment(new Date()).format("DD/MM/YYYY")}</DateText>
-        </Header>
-        <Content>
-          <GraphicContainer>
-            <HeaderText>{status.country}</HeaderText>
-            <PieChart
-              style={{ height: 200, width: 200 }}
-              valueAccessor={({ item }) => item.amount}
-              data={[
-                {
-                  key: 1,
-                  amount: status.confirmed,
-                  svg: { fill: "#00000" },
-                },
-                {
-                  key: 2,
-                  amount: status.cases,
-                  svg: { fill: "#00000" },
-                },
-                {
-                  key: 3,
-                  amount: status.deaths,
-                  svg: { fill: "#00000" },
-                },
-                {
-                  key: 4,
-                  amount: status.recovered,
-                  svg: { fill: "#00000" },
-                },
-              ]}
-              spacing={0}
-              outerRadius={"100%"}
-            />
-          </GraphicContainer>
-          <InformationContainer>
-            <InformationCard>
-              <InformationHeader>
-                <InformationColor color={colors.green} />
-                <InformationTitle>Confirmados</InformationTitle>
-              </InformationHeader>
-              <InformationNumber>{status.confirmed}</InformationNumber>
-            </InformationCard>
-
-            <InformationCard>
-              <InformationHeader>
-                <InformationColor color={colors.yellow} />
-                <InformationTitle>Suspeitos</InformationTitle>
-              </InformationHeader>
-              <InformationNumber>{status.cases}</InformationNumber>
-            </InformationCard>
-
-            <InformationCard>
-              <InformationHeader>
-                <InformationColor color={colors.red} />
-                <InformationTitle>Óbitos</InformationTitle>
-              </InformationHeader>
-              <InformationNumber>{status.deaths}</InformationNumber>
-            </InformationCard>
-
-            <InformationCard>
-              <InformationHeader>
-                <InformationColor color={colors.lightBlue} />
-                <InformationTitle>Curados</InformationTitle>
-              </InformationHeader>
-              <InformationNumber>{status.recovered}</InformationNumber>
-            </InformationCard>
-
-            <InformationCard>
-              <InformationHeader>
-                <InformationColor color={colors.darkBlue} />
-                <InformationTitle>Total</InformationTitle>
-              </InformationHeader>
-              <InformationNumber>
-                {status.confirmed +
-                  status.cases +
-                  status.deaths +
-                  status.recovered}
-              </InformationNumber>
-            </InformationCard>
-          </InformationContainer>
-          <WorldMapContainer>
-            <Image source={worldMap} style={{ height: 250, width: 250 }} />
-          </WorldMapContainer>
-          <TouchableOpacity
-            style={{
-              flexDirection: "row",
-              margin: 2,
-              alignSelf: "center",
-              alignItems: "center",
-            }}
-          >
-            <Text
-              style={{
-                color: "#3cc72a",
-                fontSize: 15,
-                textAlign: "center",
-              }}
-            >
-              BUSCAR POR ESTADO
-            </Text>
-            <Feather name="arrow-right" size={20} color="#00000" />
-          </TouchableOpacity>
-        </Content>
-      </ScrollView>
+      <View>
+        <PieChart
+          style={{ height: 200, width: 200 }}
+          valueAccessor={({ item }) => item.amount}
+          data={data}
+          spacing={0}
+          outerRadius={"100%"}
+        />
+      </View>
     </Container>
   );
 }
